1380c4cc44a92d6528a598602100a7d72f1b0f87
DESKTOP-UIP1JS1\luiz_ <luizvitorinohenrique@gmail.com>
2022-09-28 10:40:03 -0300

    adding API adding function buscar - comfirm inputs

--- js/index.js
+++ js/index.js
@@ -1,3 +1,37 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    const http = new XMLHttpRequest();
+    const url = 'https://servicodados.ibge.gov.br/api/v1/localidades/distritos?orderBy=nome';
+    http.open("GET", url);
+    http.send();
+    var select = document.getElementById("cidades");
+    http.onreadystatechange = (e) => {
+        var response = JSON.parse(http.responseText);
+        response.forEach(function (valor, indice) {
+            var opt = document.createElement("option");
+            opt.value = indice;
+            opt.innerHTML = valor.nome; // whatever property it has
+
+            // then append it to the select element
+            select.append(opt);
+        });
+    };
+});
+
+
 function conferir() {
     window.location = "./"
 };
+
+function validation() {
+    var selectCidades = document.getElementById("cidades");
+    var selectAtuacao = document.getElementById("atuacao");
+    var selectVagas = document.getElementById("vagas");
+
+    if (selectCidades.value == "default" || selectAtuacao.value == "default" || selectVagas.value == "default") {
+        alert("Selecione todas a opções antes de buscar sua vaga");
+    }
+    else {
+        alert("Busca Realizada");
+    }   
+}
